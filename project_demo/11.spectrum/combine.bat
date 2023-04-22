78619b93ef8dfa0f25eed683e8a05031a543fb75
crisqifawei <329410527@qq.com>
2023-04-22 23:48:06 +0800

    delete some v4.4 code

--- project_demo/11.spectrum/combine.bat
+++
@@ -1,3 +0,0 @@
-idf.py build
-
-esptool.py  --chip esp32s3 merge_bin -o sp_test_0x0.bin --target-offset  0x0 --flash_mode dio --flash_freq 80m 0x0 build\bootloader\bootloader.bin 0x8000 build\partition_table\partition-table.bin 0x10000 build/hello-world.bin 
